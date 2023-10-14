a082a91c0c7099887b241659300c179daa3777e7
piscis <self@pirsig.net>
2023-10-14 14:25:19 +0200

    modify uno.config.ts to show none working example

--- uno.config.ts
+++ uno.config.ts
@@ -7,23 +7,27 @@ export default defineConfig({
   content: {
     inline: [
       async () => {
-        const ptStyles = './node_modules/primevue/passthrough/tailwind/index.js';
-        const ptExists = fs.existsSync(ptStyles);
-        if(ptExists === true) {
-          const content = await fsp.readFile(ptStyles);
-          return content.toString();
-        } else {
+        // UNCOMMENT TO SEE HOW IT WORKS
+        // Utility styles gets inlined to workaround the problem
+        
+        //const ptStyles = './node_modules/primevue/passthrough/tailwind/index.js';
+        //const ptExists = fs.existsSync(ptStyles);
+        //if(ptExists === true) {
+        //  const content = await fsp.readFile(ptStyles);
+        //  return content.toString();
+        //} else {
           return '';
-        }
+        //}
       }
     ],
     pipeline: {
       include: [
         // the default
         /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // This line does not work it should include 
+        // node_modules/primevue/passthrough/tailwind/index.js
+        // Which contains utility styles that are used for the passthough
         "./node_modules/primevue/**/*.{vue,js,ts,jsx,tsx}",
-        // include js/ts files
-        './formkit.config.ts',
       ],
       // exclude files
       // exclude: []
